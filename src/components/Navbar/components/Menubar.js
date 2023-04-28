32d890e1c42b631a3adac3401c72cf97dda5958c
Usama <um3121997@gmail.com>
2023-04-28 17:52:46 +0500

    UI settings Navbar

--- src/components/Navbar/components/Menubar.js
+++ src/components/Navbar/components/Menubar.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import  React,{useState} from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
@@ -8,10 +8,21 @@ import MenuIcon from '@mui/icons-material/Menu';
 import './navbar.css'
 import { Button, Menu, MenuItem, TextField } from '@mui/material';
 import Fade from '@mui/material/Fade';
+import ListOutlinedIcon from '@mui/icons-material/ListOutlined';
+import { List, ListItem, ListItemIcon, ListItemText, Popover } from '@mui/material';
 
+
+const categories = [
+    { name: 'Category 1', value: 'category-1' },
+    { name: 'Category 2', value: 'category-2' },
+    { name: 'Category 3', value: 'category-3' },
+  ];
+
+  
 export default function ManuBar() {
 
-    const [anchorEl, setAnchorEl] = React.useState(null);
+    const [anchorEl, setAnchorEl] = useState(null);
+    const [selectedValue, setSelectedValue] = useState('');
     const open = Boolean(anchorEl);
     const handleClick = (event) => {
       setAnchorEl(event.currentTarget);
@@ -19,35 +30,58 @@ export default function ManuBar() {
     const handleClose = () => {
       setAnchorEl(null);
     };
+
+    const handleListItemClick = (event, value) => {
+        setSelectedValue(value);
+        handleClose();
+      };
   
   return (
     <Box sx={{ flexGrow: 1 }} >
       <AppBar position="static" >
         <Toolbar  className="topBarMenuBar">
-            <div>
-        <Button
-        id="fade-button"
-        aria-controls={open ? 'fade-menu' : undefined}
-        aria-haspopup="true"
-        aria-expanded={open ? 'true' : undefined}
-        onClick={handleClick}
-      >
-        Dashboard
+            <div  className="categoriesBtn">
+            <Button
+            // id="fade-button"
+            // aria-controls={open ? 'fade-menu' : undefined}
+            // aria-haspopup="true"
+            // aria-expanded={open ? 'true' : undefined}
+            style={{width:"278px"}}
+            size="large" 
+            onClick={handleClick}
+            startIcon={<ListOutlinedIcon sx={{mr:2}} />}
+            endIcon={<ListOutlinedIcon />}
+        >
+        {selectedValue || 'Select an option'}
       </Button>
-      <Menu
-        id="fade-menu"
-        MenuListProps={{
-          'aria-labelledby': 'fade-button',
-        }}
+      <Popover
+        open={Boolean(anchorEl)}
         anchorEl={anchorEl}
-        open={open}
         onClose={handleClose}
-        TransitionComponent={Fade}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'left',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'left',
+        }}
+        className="my-popover"
       >
-        <MenuItem onClick={handleClose}>Profile</MenuItem>
-        <MenuItem onClick={handleClose}>My account</MenuItem>
-        <MenuItem onClick={handleClose}>Logout</MenuItem>
-      </Menu>
+           <List className="my-list">
+          {categories.map((category) => (
+            <ListItem
+              key={category.value}
+              button
+              onClick={(event) => handleListItemClick(event, category.value)}
+              selected={selectedValue === category.value}
+              className="my-list-item"
+            >
+              <ListItemText primary={category.name} />
+            </ListItem>
+          ))}
+        </List>
+      </Popover>
       </div>
       <div className="topBarMenuBarItems">
           <Typography variant="h6" color="inherit" component="div">
--- src/components/Navbar/components/TopBar.js
+++ src/components/Navbar/components/TopBar.js
@@ -7,8 +7,29 @@ import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import './navbar.css'
 import { TextField } from '@mui/material';
+import AdbIcon from '@mui/icons-material/Adb';
 import ManuBar from './Menubar';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import { Select, MenuItem } from '@mui/material';
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
+import LocalMallOutlinedIcon from '@mui/icons-material/LocalMallOutlined';
+
+const languageOptions = [
+    { value: 'en', label: 'English' },
+    { value: 'fr', label: 'French' },
+    { value: 'es', label: 'Spanish' },
+    // add more language options here
+  ];
+  
 export default function DenseAppBar() {
+    const [selectedLanguage, setSelectedLanguage] = React.useState('en');
+
+    const handleChange = (event) => {
+        setSelectedLanguage(event.target.value);
+    };
+
   return (
     <Box sx={{ flexGrow: 1 }} >
       <AppBar position="static" >
@@ -17,17 +38,50 @@ export default function DenseAppBar() {
             <MenuIcon />
           </IconButton> */}
           <Typography variant="h6" color="inherit" component="div">
-            Photos
+            +92 000088888999
           </Typography>
+          <div style={{display:"flex", alignItems:"center"}}>
+           <Select className="topBarSelect" value={selectedLanguage} onChange={handleChange} defaultValue="en">
+                {languageOptions.map((option) => (
+                    <MenuItem key={option.value} value={option.value}>
+                    {option.label}
+                    </MenuItem>
+                ))}
+            </Select>
+            <FacebookIcon   className="topNavIcons" />
+            <InstagramIcon  className="topNavIcons"/>
+            <TwitterIcon className="topNavIcons" />
+          </div>
         </Toolbar>
         <Toolbar  className="topBarSearch">
+            <div  style={{display:"flex", alignItems:"center"}}>
+        <AdbIcon 
+        sx={{
+            //  display: { xs: 'none', md: 'flex' },
+              mr: 1 }} 
+        />
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex' },
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            cartMate
+          </Typography>
+          </div>
+          <TextField  className="topBarSearchField" placeholder='Search...' variant="outlined" style={{borderRadius:"50%"}}/>
           <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
-            <MenuIcon />
-          </IconButton>
-          <TextField className="topBarSearchField"/>
-          <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
-            <MenuIcon sx={{ mr: 2 }} />
-            <MenuIcon />
+            <AccountCircleOutlinedIcon sx={{ mr: 2 }}  />
+            <LocalMallOutlinedIcon />
           </IconButton>
           
         </Toolbar>
